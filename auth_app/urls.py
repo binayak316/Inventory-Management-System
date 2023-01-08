c299c2c715444521c06f371a0a100e4d7689934b
binayak316 <binayakpokhrel005@gmail.com>
2023-01-08 17:09:31 +0545

    login done

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -3,4 +3,6 @@ from auth_app import views
 
 urlpatterns = [
     path('register/', views.register_page, name="register-page"),
+    path('login/',views.login_page, name="login-page"),
+    path('logout-user/', views.logout_page, name='logout-page'),
 ]
