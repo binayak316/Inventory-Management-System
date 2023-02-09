7404d9c05c1292b0e48a748535db806133b4a23f
binayak316 <binayakpokhrel005@gmail.com>
2023-02-09 17:37:23 +0545

    chart without ajax

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -8,25 +8,16 @@ from rest_framework_simplejwt.views import (
 # app_name = 'auth_app'
 
 urlpatterns = [
+    # web
     path('', views.index, name="index"),
     path('register/', views.register_page, name="register-page"),
     path('login/',views.login_page, name="login-page"),
     path('logout-user/', views.logout_page, name='logout-page'),
-
     path('password-reset/<str:target>/',views.password_reset_page, name='password-reset' ),
     path('password-reset-confirm/',views.password_reset_confirm_page, name='password-reset-confirm' ),
-
-
-
-
-
     # otp form
     path('check_otp/<int:user_id>', views.check_otp, name='check_otp'),
 
-
-    # charts
-    # path('chart-index/',views.chart_index, name="chart-index" ),
-    path('ChartData/', ChartData.as_view(), name="ChartData"),
 
     # auth_app api's
     path('api/register/', UserRegistrationApi.as_view(), name='register-api'),
@@ -36,13 +27,15 @@ urlpatterns = [
     path('api/passwordresetconfirm/<int:user_id>/', PasswordResetConfirmApi.as_view(), name='passwordresetconfrim'),
     path('api/changepassword/', ChangePasswordApi.as_view(), name='changepassword'),
 
-
     # token for refresh and token generation
-    
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 
+    # charts
+    path('ChartData/', ChartData.as_view(), name="ChartData"),# shows the categories and products chart
+
+
+    
 
 
 ]
