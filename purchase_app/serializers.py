974e836227accb7c0e5ef859c363063bbe691dac
MrJung988 <karkisamir99@gmail.com>
2023-02-13 18:56:06 +0545

    response handling, retrieve name of staff, vendor name and price of product in sales and purchase

--- purchase_app/serializers.py
+++ purchase_app/serializers.py
@@ -1,15 +1,22 @@
 from rest_framework import serializers
 from .models import Purchase, PurchaseItem
 from drf_writable_nested import WritableNestedModelSerializer
+from product_app.serializers import ProductSpecificSerializer
+from third_party.serializers import VendorSpecificSerializer
+
 
 class PurchaseItemSerializer(serializers.ModelSerializer):
+    product = ProductSpecificSerializer()
+
     class Meta:
         model = PurchaseItem
         fields = '__all__'
 
 
 class PurchaseSerializer(WritableNestedModelSerializer ,serializers.ModelSerializer):
+    purchase_by_name = serializers.StringRelatedField(source='purchased_by.username', read_only=True)
     purchase_items = PurchaseItemSerializer(many=True)
+    vendor = VendorSpecificSerializer()
     class Meta:
         model = Purchase
         fields = '__all__'
--- third_party/serializers.py
+++ third_party/serializers.py
@@ -7,7 +7,17 @@ class VendorSerializer(serializers.ModelSerializer):
         model =Vendor
         fields = '__all__'
 
+class VendorSpecificSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Vendor
+        fields =['name']
+
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
+
+class CustomerSpecificSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['name']
